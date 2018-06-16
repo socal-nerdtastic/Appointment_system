abcaa2ca8ef5c3ef589f54c7b0c28d24db99fe60
socalNerdtastic <socal.nerdtastic@gmail.com>
2018-06-15 19:26:58 -0700

    added subclassing

--- login.py
+++ login.py
@@ -5,58 +5,63 @@ import tkinter as tk
 import tkinter.messagebox
 from menu import Menu
 
-class Login():
-    def __init__(self,master):
-        self.master = master
-        # master is the frame or whole interface. Frame is the section of the whole screen you've chosen
-        self.master = tk.Frame(master, width=500, height=500,bg='#ffffff')
-        self.master.pack()
+class Login(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master, width=500, height=500,bg='#ffffff')
+
         # administrator text
-        self.heading = tk.Label(self.master,text="ADMINISTRATOR",font=('arial 35 bold'),fg='black',bg='#ffffff')
+        self.heading = tk.Label(self,text="ADMINISTRATOR",font=('arial 35 bold'),fg='black',bg='#ffffff')
         self.heading.place(x=55,y=0)
         # sign in to get access text
-        self.access = tk.Label(self.master,text="Please Sign In to get access",font=('arial 20'),fg='black',bg='#ffffff')
+        self.access = tk.Label(self,text="Please Sign In to get access",font=('arial 20'),fg='black',bg='#ffffff')
         self.access.place(x=80,y=50)
         # UserId label
-        self.user_ID = tk.Label(self.master,text="User ID: ",font=('arial 14'),fg="black",bg="#ffffff")
+        self.user_ID = tk.Label(self,text="User ID: ",font=('arial 14'),fg="black",bg="#ffffff")
         self.user_ID.place(x=40,y=223)
         # Password label
-        self.password = tk.Label(self.master,text="Password: ",font=('arial 14'),fg="black",bg="#ffffff")
+        self.password = tk.Label(self,text="Password: ",font=('arial 14'),fg="black",bg="#ffffff")
         self.password.place(x=40,y=263)
         # UserId entry box
-        self.user_ID_ent = tk.Entry(self.master,width=40)
+        self.user_ID_ent = tk.Entry(self,width=40)
         self.user_ID_ent.place(x=150,y=228)
         # Password entry box
-        self.password_ent = tk.Entry(self.master,width=40)
+        self.password_ent = tk.Entry(self,width=40)
         self.password_ent.place(x=150,y=268)
         # gives user the user id and password
         # userid answer
-        self.user_ans = tk.Label(self.master,text="UserID: admin",bg='white')
+        self.user_ans = tk.Label(self,text="UserID: admin",bg='white')
         self.user_ans.place(x=40,y=423)
         # password answer
-        self.pass_ans = tk.Label(self.master,text="Password: admin",bg='white')
+        self.pass_ans = tk.Label(self,text="Password: admin",bg='white')
         self.pass_ans.place(x=40,y=453)
         # login button *** no cmd yet! ***
-        self.login = tk.Button(self.master, text="Login", width=33, bg='#b3b3ff', command=self.login_validation)
+        self.login = tk.Button(self, text="Login", width=33, bg='#b3b3ff', command=self.login_validation)
         self.login.place(x=153, y=303)
     def login_validation(self):
         if (self.user_ID_ent.get() == 'admin') and (self.password_ent.get() == 'admin'):  # go to menu
             print('success!')
             # destroying all previous Tkinter data in order to create an entirely new layout
             # opens main menu()
-            master = Menu(root)
-            my_menu = Menu(self.master)
+            master = Menu(self.master)
+            master.pack()
+            self.destroy()
         else:
             tkinter.messagebox.showinfo("Invalid Credentials", "Answers are in menu")
 
-# store Tk() in root object. Root is a blank window!
-root = tk.Tk() # the window!
-# store b as an instance of root
-b = Login(root)
-# size of root
-root.geometry("500x500+0+0")
-# cant be resized
-root.resizable(False, False)
-# just says that i will put your window in an infinite loop until you
-# close the window yourself
-root.mainloop()
+def main():
+    # store Tk() in root object. Root is a blank window!
+    root = tk.Tk() # the window!
+    # store b as an instance of root
+    b = Login(root)
+    b.pack()
+    # size of root
+    root.geometry("500x500+0+0")
+    # cant be resized
+    root.resizable(False, False)
+    # just says that i will put your window in an infinite loop until you
+    # close the window yourself
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
--- menu.py
+++ menu.py
@@ -2,24 +2,21 @@
 
 import tkinter as tk
 
-class Menu():
-    def __init__(self,master):
-        self.master = master
-        # master is the frame or whole interface. Frame is the section of the whole screen you've chosen
-        self.master = tk.Frame(self.master, width=500, height=500, bg='#ffffff')
-        self.master.pack()
+class Menu(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master, width=500, height=500, bg='#ffffff')
         # after these lines, it becomes a blank slate once again
         # title header
-        self.clinic_header = tk.Label(self.master, text="+ Stephen's Clinic +", font=('arial 30'), fg='red', bg='white')
+        self.clinic_header = tk.Label(self, text="+ Stephen's Clinic +", font=('arial 30'), fg='red', bg='white')
         self.clinic_header.place(x=80, y=0)
         # new patient button *** add cmd ***
-        self.new_patient = tk.Button(self.master, text="Add a new patient", width=40, height=5, bg="#9999ff",command=self.add_patient)
+        self.new_patient = tk.Button(self, text="Add a new patient", width=40, height=5, bg="#9999ff",command=self.add_patient)
         self.new_patient.place(x=105, y=130)
         # update patient button *** add cmd ***
-        self.update_patient = tk.Button(self.master, text="Update/Delete Patients", width=40, height=5, bg="#9999ff",command=self.change_patient)
+        self.update_patient = tk.Button(self, text="Update/Delete Patients", width=40, height=5, bg="#9999ff",command=self.change_patient)
         self.update_patient.place(x=105, y=250)
         # schedule patient button *** add cmd ***
-        self.schedule_patient = tk.Button(self.master, text="View Schedule", width=40, height=5, bg="#9999ff",command=self.view_schedule)
+        self.schedule_patient = tk.Button(self, text="View Schedule", width=40, height=5, bg="#9999ff",command=self.view_schedule)
         self.schedule_patient.place(x=105, y=370)
     def add_patient(self):
             pass
@@ -28,13 +25,18 @@ class Menu():
     def view_schedule(self):
             pass
 
-if __name__ == "__main__":
+def main():
+    """this is just for debugging, the normal entry point is in Login.py"""
     root = tk.Tk()
     # size of root
     b = Menu(root)
+    b.pack()
     root.geometry("500x500+0+0")
     # cant be resized
     root.resizable(False, False)
     # just says that i will put your window in an infinite loop until you
     # close the window yourself
     root.mainloop()
+
+if __name__ == "__main__":
+    main()
